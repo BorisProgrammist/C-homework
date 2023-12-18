e8981f55a66bf51342ad78ade18308a67a2fb9a9
Boris Bespalov <Bespal.bo58@gmail.com>
2023-12-18 22:05:17 +0500

    Создали репозиторий

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+        
+        int[] array = { 45, 22, 78, 5, 90, 30, 15, 75, 12, 50 };
+
+        int countInRange = 0;
+
+        // Находим количество элементов в отрезке [20, 90]
+        foreach (int n in array)
+        {
+            if (n >= 20 && n <= 90)
+            {
+                Console.Write($"{n} ");
+                countInRange++;
+            }
+        }
+
+        Console.WriteLine($"\nКоличество элементов в отрезке [20, 90]: {countInRange}");
+
+        Console.ReadLine(); 
+    
+
