fb9374f3da1ead16d186a2b7a24a8d9f0974d618
Boris Bespalov <Bespal.bo58@gmail.com>
2023-12-18 22:10:42 +0500

    Решили вторую задачу

--- Program.cs
+++ Program.cs
@@ -1,21 +1,46 @@
 ﻿
-        
-        int[] array = { 45, 22, 78, 5, 90, 30, 15, 75, 12, 50 };
+    //    Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, 
+    //    значения которых лежат в отрезке [20,90]. 
+       
+    //     int[] array = { 45, 22, 78, 5, 90, 30, 15, 75, 12, 50 };
 
-        int countInRange = 0;
+    //     int countInRange = 0;
 
-        // Находим количество элементов в отрезке [20, 90]
+    //     // Находим количество элементов в отрезке [20, 90]
+    //     foreach (int n in array)
+    //     {
+    //         if (n >= 20 && n <= 90)
+    //         {
+    //             Console.Write($"{n} ");
+    //             countInRange++;
+    //         }
+    //     }
+
+    //     Console.WriteLine($"\nКоличество элементов в отрезке [20, 90]: {countInRange}");
+
+    //     Console.ReadLine(); 
+    //-------------------------------------------------------------------------------------------------------------------------
+
+    // Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+         int[] array = { 5, 12, 7, 30, 42, 18, 63, 91, 20, 8 };
+
+        Console.WriteLine("Массив:");
         foreach (int n in array)
         {
-            if (n >= 20 && n <= 90)
+            Console.Write($"{n} ");
+        }
+        Console.WriteLine();
+        int countEvenNumbers = 0;
+        foreach (int n in array)
+        {
+           
+            if (n % 2 == 0)
             {
-                Console.Write($"{n} ");
-                countInRange++;
+                countEvenNumbers++;
             }
         }
-
-        Console.WriteLine($"\nКоличество элементов в отрезке [20, 90]: {countInRange}");
-
+        Console.WriteLine($"Количество чётных чисел в массиве: {countEvenNumbers}");
         Console.ReadLine(); 
     
 
