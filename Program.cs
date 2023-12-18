9c9eeac279a0bfe99a38e0ecbceaf672de7dd814
Boris Bespalov <Bespal.bo58@gmail.com>
2023-12-18 22:23:50 +0500

    Решили вторую задачу 3

--- Program.cs
+++ Program.cs
@@ -23,24 +23,67 @@
 
     // Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
 
-         int[] array = { 5, 12, 7, 30, 42, 18, 63, 91, 20, 8 };
+        //  int[] array = { 5, 12, 7, 30, 42, 18, 63, 91, 20, 8 };
+
+        // Console.WriteLine("Массив:");
+        // foreach (int n in array)
+        // {
+        //     Console.Write($"{n} ");
+        // }
+        // Console.WriteLine();
+        // int countEvenNumbers = 0;
+        // foreach (int n in array)
+        // {
+           
+        //     if (n % 2 == 0)
+        //     {
+        //         countEvenNumbers++;
+        //     }
+        // }
+        // Console.WriteLine($"Количество чётных чисел в массиве: {countEvenNumbers}");
+        // Console.ReadLine(); 
+    // ----------------------------------------------------------------------------------------------------------------------------
+
+// Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+
+        double[] array = { 12.45, 8.32, 15.76, 11.11, 19.28, 14.55, 9.87, 20.36, 18.49, 13.72 };
 
         Console.WriteLine("Массив:");
-        foreach (int n in array)
+
+      
+        foreach (double n in array)
         {
             Console.Write($"{n} ");
         }
         Console.WriteLine();
-        int countEvenNumbers = 0;
-        foreach (int n in array)
+
+        double maxNumber = array[0];
+        double minNumber = array[0];
+
+     
+        foreach (double n in array)
         {
-           
-            if (n % 2 == 0)
+          
+            if (n > maxNumber)
             {
-                countEvenNumbers++;
+                maxNumber = n;
+            }
+
+            
+            if (n < minNumber)
+            {
+                minNumber = n;
             }
         }
-        Console.WriteLine($"Количество чётных чисел в массиве: {countEvenNumbers}");
+        double difference = maxNumber - minNumber;
+        Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
         Console.ReadLine(); 
     
+
+
+
+
+
 
